8bea5c488260ae788b454ae23ebc9c6ef2696261
phoenix <olatemijutosin@gmail.com>
2025-06-26 14:19:20 -0700

    Learning javascript

--- page-javascript/index.js
+++ page-javascript/index.js
@@ -7,8 +7,12 @@
 // for (const button of buttons) {
 //   button.addEventListener("click", createParagraph);
 // }
+const buttons = document.querySelectorAll("button");
 function createParagraph() {
   const para = document.createElement("p");
   para.textContent = "You clicked the button!";
   document.body.appendChild(para);
+}
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
 }
